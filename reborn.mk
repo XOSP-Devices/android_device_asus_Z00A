c9bb51c803dffea03f2999d34002c3375669059e
Calin <nilac8991@gmail.com>
2015-12-07 13:24:35 +0100

    Use Reborn vendor files
    Change-Id: I01e030972c810b7c6e1fa634ce0b43847d45e90a

--- reborn.mk
+++ reborn.mk
@@ -7,6 +7,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 $(call inherit-product, vendor/reborn/config/common_full_phone.mk)
 
+$(call inherit-product, vendor/reborn/config/reborn.mk)
+
 # Inherit device configuration
 $(call inherit-product, device/asus/Z00A/device.mk)
 
