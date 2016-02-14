3d0dd8088d2e1eba7f752992a505092bc0d987a4
nilac8991 <nilac8991@gmail.com>
2016-02-14 12:09:54 +0100

    Refactor to XOSP
    Signed-off-by: nilac8991 <nilac8991@gmail.com>

--- xosp.mk
+++ xosp.mk
@@ -2,30 +2,30 @@
 TARGET_SCREEN_HEIGHT := 1080
 TARGET_SCREEN_WIDTH := 1920
 
-# Inherit some common REBORN stuff.
+# Inherit some common XOSP stuff.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-$(call inherit-product, vendor/reborn/config/common_full_phone.mk)
+$(call inherit-product, vendor/xosp/config/common_full_phone.mk)
 
-$(call inherit-product, vendor/reborn/config/reborn.mk)
+$(call inherit-product, vendor/xosp/config/xosp.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/asus/Z00A/device.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/reborn/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/xosp/config/nfc_enhanced.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/asus/Z00A/overlay
 
 PRODUCT_RUNTIMES := runtime_libart_default
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := reborn_Z00A
+PRODUCT_NAME := xosp_Z00A
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := ASUS_Z00A
 PRODUCT_MANUFACTURER := asus
 PRODUCT_DEVICE := Z00A
-RELEASE_TYPE := REBORN_OFFICIAL
+RELEASE_TYPE := XOSP_OFFICIAL
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=WW_Z00A \
